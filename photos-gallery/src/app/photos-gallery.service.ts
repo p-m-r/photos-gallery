2f6ad67973bfb9bb241346439bc3e1a4eceb8b3c
p-m-r <p.m.rduch@gmail.com>
2018-08-14 17:40:51 +0200

    captions

--- photos-gallery/src/app/photos-gallery.service.ts
+++ photos-gallery/src/app/photos-gallery.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
-const FLICKR_URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b58ac82567c0526d9d82a54b2d3db276&format=json&per_page=10&tags=dog";
+const FLICKR_URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=b58ac82567c0526d9d82a54b2d3db276&format=json&per_page=10&tags=dog&extras=description,date_taken,owner_name";
 
 @Injectable()
 export class PhotosGalleryService {
